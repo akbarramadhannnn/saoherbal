9e6ecdc6bba9ee89e77609aca2572d78546a06b9
akbarramadhannnn <akbarramadhan452@gmail.com>
2022-02-17 15:03:13 +0700

    update env backend

--- backend/config/env.js
+++ backend/config/env.js
@@ -2,14 +2,14 @@ const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
   server: {
-    port: NODE_ENV === "development" ? 2000 : "",
+    port: NODE_ENV === "development" ? 2000 : 2000,
   },
   database_sql: {
-    host: NODE_ENV === "development" ? "localhost" : "",
-    port: NODE_ENV === "development" ? 3306 : "",
-    user: NODE_ENV === "development" ? "root" : "",
-    password: NODE_ENV === "development" ? "123pitik" : "",
-    database: NODE_ENV === "development" ? "dev_saoherbal" : "",
+    host: NODE_ENV === "development" ? "localhost" : "185.210.144.158",
+    port: NODE_ENV === "development" ? 3306 : 3306,
+    user: NODE_ENV === "development" ? "root" : "akbar",
+    password: NODE_ENV === "development" ? "123pitik" : "123akbar",
+    database: NODE_ENV === "development" ? "dev_saoherbal" : "dev_saoherbal",
   },
   api_image: {
     url_api_v1:
