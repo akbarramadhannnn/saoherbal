21c1786b1ce35cfa98b061a3022a21a06a236bf6
akbarramadhannnn <akbarramadhan452@gmail.com>
2022-02-21 01:25:41 +0700

    update

--- api-file/routes/api/v1/index.js
+++ api-file/routes/api/v1/index.js
@@ -4,7 +4,11 @@ const {
   deleteSingleImage,
   generateFilePdf,
 } = require("../../../controller/file");
+const Response = require("../../../helpers/response");
 
+router.get("/", (req, res) => {
+  return res.json(Response(true, 200, `root api file`, {}));
+});
 router.post("/upload-single-image", uploadSingleImage);
 router.post("/generate-pdf-invoice", generateFilePdf);
 router.delete("/delete-single-image/:type/:name", deleteSingleImage);
