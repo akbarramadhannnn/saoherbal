469d14113d3c44b2ee34008ea27775800e3b3bb0
akbarramadhannnn <akbarramadhan452@gmail.com>
2022-03-10 13:22:49 +0700

    update

--- company-profile/src/components/Home/H1ChooseUsSection.js
+++ company-profile/src/components/Home/H1ChooseUsSection.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
-import Image from "next/image";
-import whyShape from "../../../public/static/assets/img/shape/why-shape.png";
-import chooseImage1 from "../../../public/static/assets/img/Choose/01.jpg";
-import icon1 from "../../../public/static/assets/img/icon/1.png";
-import icon2 from "../../../public/static/assets/img/icon/2.png";
-import icon3 from "../../../public/static/assets/img/icon/3.png";
 
 class H1ChooseUsSection extends Component {
   render() {
     return (
       <div className="choose-us-area pt-100 pb-70 pos-relative">
         <div className="shape spahe2 bounce-animate">
-          <Image src={whyShape} alt="gambar" />
+          <img
+            src={require("../../../public/static/assets/img/shape/why-shape.png")}
+            alt="gambar"
+          />
         </div>
         <div className="container">
           <div className="row">
             <div className="col-xl-7 col-lg-12">
               <div className="choose-img mb-30">
-                <Image src={chooseImage1} alt="picture" />
+                <img
+                  src={require("../../../public/static/assets/img/Choose/01.jpg")}
+                  alt="picture"
+                />
               </div>
             </div>
             <div className="col-xl-5 col-lg-12">
@@ -32,7 +32,7 @@ class H1ChooseUsSection extends Component {
                 </div>
                 <div className="choose-content mb-40">
                   <div className="choose-us-img">
-                    <Image src={icon1} alt="picture" />
+                    <img src={require('../../../public/static/assets/img/icon/1.png')} alt="picture" />
                   </div>
                   <div className="choose-text">
                     <h4>Proses yang mudah</h4>
@@ -44,7 +44,7 @@ class H1ChooseUsSection extends Component {
                 </div>
                 <div className="choose-content mb-40">
                   <div className="choose-us-img">
-                    <Image src={icon2} alt="picture" />
+                    <img src={require('../../../public/static/assets/img/icon/2.png')} alt="picture" />
                   </div>
                   <div className="choose-text">
                     <h4>100% Terpercaya</h4>
@@ -56,7 +56,7 @@ class H1ChooseUsSection extends Component {
                 </div>
                 <div className="choose-content">
                   <div className="choose-us-img">
-                    <Image src={icon3} alt="picture" />
+                    <img src={require('../../../public/static/assets/img/icon/3.png')} alt="picture" />
                   </div>
                   <div className="choose-text">
                     <h4>Produk Terbaik</h4>
